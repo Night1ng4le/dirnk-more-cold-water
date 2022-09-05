04caa6dc7befbf6edc1c63518fa536748cca6521
Night1ng4le <2276811587@qq.com>
2022-09-05 11:03:08 +0800

    9.5

--- PngGenerateThread.py
+++ PngGenerateThread.py
@@ -1,5 +1,7 @@
+# -*- coding: utf-8 -*-
+
 import scipy.io as scio #这是v3.7之前的版本需要用的格式
-import hdf5storage as hdf
+# import hdf5storage as hdf
 import pandas as pd
 import numpy as np
 import scipy
@@ -22,35 +24,38 @@ def gen(folder_path, save_PATH, file_names, folder):
 #        print(file_name)
         name, suffix = os.path.splitext(file_name)
         # print(name)
-        src_path = folder_path + '/' + file_name
-        # print(src_path)
-        # save_path = des_PATH + folder +name
-        save_path = save_PATH + folder + '/' + name + '.png'
-        # print(save_path)
-        array = np.load(src_path, allow_pickle=True)
-        # # print(arr)
-        # # plt.figure()
-        array_normal = min_max_scaler.fit_transform(array)
-        colors = array_normal[:,1]
-        plt.scatter(array_normal[:,0],array_normal[:,1],marker = 'o',alpha = 0.5, c = colors,cmap='viridis')
-        plt.savefig(save_path)
-        # plt.show()
-        plt.close()
+        if(suffix == '.npy'):
+            # print(name)
+            src_path = folder_path + '/'+ file_name
+            # print(src_path)
+            # save_path = des_PATH + folder +name
+            save_path = save_PATH +folder+'/'+name+'.png'
+            # print(save_path)
+            array = np.load(src_path)
+            # print(array.shape)
+            colors = array[:,1]
+            plt.scatter(array[:,0],array[:,1],marker = 'o',alpha = 0.5, c = colors,cmap='viridis', s=1)
+            plt.xlim((0, 2.75))
+            plt.ylim((0, 4))
+            plt.savefig(save_path)
+            # plt.show()
+            plt.close()
 if __name__ == '__main__':
     # 读取每个类别下的完整样本
-    filePath = '/Volumes/Seagate/graduate/data-backup/Spatial/slice_data/4db/'
+    filePath = '/Volumes/Seagate/data/28db/'
     folder_names = readname(filePath) #这里一定要使用绝对路径
     # del folder_names[0]
-    print(folder_names)
+    # print(folder_names)
 
     # 循环生成图片样本
-    save_PATH = '/Users/hurrican/Desktop/experiment/Spatial/slice_png/30db/'
-    folder_name = folder_names[1:6]+folder_names[7:]
-    min_max_scaler = preprocessing.MinMaxScaler()
+    save_PATH = '/Volumes/Seagate/single_slice_png/test/28db/'
+    folder_name = folder_names[0:3]+folder_names[7:12]+folder_names[13:]
+    # min_max_scaler = preprocessing.MinMaxScaler()
+    print(folder_name)
     threads = []
     i = 0
     for folder in folder_name:
-        # print(folder)
+        print(folder)
         folder_path = filePath + folder
         # print(folder_path)
         file_names = readname(folder_path)
